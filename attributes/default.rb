346736055249705ddfaf67c9ce54e2984ce6e986
Brett Richardson <Brett.Richardson.NZ@gmail.com>
2013-08-15 23:13:55 +0200

    Initial project commit

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['composer']['global']      = true
+default['composer']['prefix']      = '/usr/local'
+default['composer']['url']         = 'https://getcomposer.org/installer'
+default['composer']['install_dir'] = nil
---
+++ metadata.rb
@@ -0,0 +1,17 @@
+name             'php-composer'
+maintainer       'Brett Richardson'
+maintainer_email 'brett.Richardson.nz@gmail.com'
+license          'MIT'
+description      'Installs/Configures Composer'
+long_description IO.read File.join( File.dirname( __FILE__ ), 'README.md' )
+version          '0.0.1'
+
+%w{ debian ubuntu redhat centos fedora scientific amazon }.each do |os|
+	supports os
+end
+
+
+depends 'php'
+
+
+recipe 'install', 'Installs composer.'
